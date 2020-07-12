75b61ccf4977b2fb4d6a52830246fe266e30f7a4
NISHANTH <nishanthebbar@gmail.com>
2020-07-12 23:15:53 +0530

    What Are You Designing - Design the Gear class. 					Design Decisions - DRY 					Implementation Decisions - Use of attributes like cog, chainRing 					ToDo List -Convert to Single Responsibiity methods

---
+++ p1/gear.java
@@ -0,0 +1,77 @@
+package p1;
+
+
+interface Gear{
+    
+
+    double ratio();
+
+}
+
+
+class SimpleGear implements Gear{
+    private int cog;
+    private int chainRing;
+    private double ratio;
+    private double gearInches;
+    Wheel newWheel;
+
+
+    public double ratio(){
+        return (double)getChainRing() / getCog();
+    }
+
+    double gearInches(Wheel newWheel){
+        return getRatio() * diameter();
+    }
+
+    SimpleGear(int cog, int chainRing){
+        setCog(cog);
+        setChainRing(chainRing);
+        setRatio(ratio());
+    }
+
+    SimpleGear(int cog, int chainRing, int rim, int tire){
+        setCog(cog);
+        setChainRing(chainRing);
+        newWheel = new Wheel(rim, tire);
+        setGearInches(gearInches(newWheel));  
+    }
+
+
+    public double diameter(){
+        return newWheel.diameter();
+    }
+
+    public int getCog() {
+        return cog;
+    }
+
+    public void setCog(int cog) {
+        this.cog = cog;
+    }
+
+    public int getChainRing() {
+        return chainRing;
+    }
+
+    public void setChainRing(int chainRing) {
+        this.chainRing = chainRing;
+    }
+
+    public double getRatio() {
+        return ratio;
+    }
+
+    public void setRatio(double ratio) {
+        this.ratio = ratio;
+    }
+
+    public double getGearInches() {
+        return gearInches;
+    }
+
+    public void setGearInches(double gearInches) {
+        this.gearInches = gearInches;
+    }
+}
